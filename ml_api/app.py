1558b6bb1ae5c3fb90a4281f6606dc255b4a5a85
Mike-Soukup <michaeltsoukup@gmail.com>
2023-08-12 16:27:23 +0000

    Home Page for Flask API Up. Need to add Image Prediction Functionality.

---
+++ ml_api/app.py
@@ -0,0 +1,19 @@
+"""Flask App for ML API"""
+import os
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import json
+
+# Define upload folder path:
+UPLOAD_FOLDER = os.path.join("static",'uploads')
+RESPONSE_FOLDER = os.path.join("response")
+# Define allowed files:
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESPONSE_FOLDER'] = RESPONSE_FOLDER
+
+@app.route("/")
+def welcome():
+    """Fashion MNIST API Landing Page."""
+    return render_template("home.html")
